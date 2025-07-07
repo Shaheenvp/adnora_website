ef756c796a605845e569279fb137a0be7c361740
Mohammed Shaheen <Mohammedshaheen109@gmail.com>
2025-07-07 17:44:59 +0000

    increase some more the size of the logo in the navabr left side in the t

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -36,7 +36,7 @@ export function Header() {
     >
       <div className="container mx-auto flex h-20 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center gap-2" aria-label="Adnora Home">
-          <Logo />
+          <Logo className="h-14" />
         </Link>
         <nav className="hidden items-center gap-6 md:flex">
           {navLinks.map((link) => (
